c4118ba98632a45340321cb955fdad85d0c6bd80
Seyed Pouria Mirelmi <pouria.mirelmi@gmail.com>
2023-08-30 16:41:04 +0200

    Upload New File

---
+++ C++ code and python wrapper/bin/c4che/build.config.py
@@ -0,0 +1,2 @@
+version = 0x2001800
+tools = [{'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'cxx', 'tooldir': None, 'funs': None}, {'tool': 'clang++', 'tooldir': None, 'funs': None}, {'tool': 'compiler_cxx', 'tooldir': None, 'funs': None}, {'tool': 'unittest_gtest', 'tooldir': None, 'funs': None}]
